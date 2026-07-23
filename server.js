e5b47f71e489276fcde5fe4bd33a4a9a1e02e387
Rahul Sharma <rahulbhindamishra@gmail.com>
2026-07-23 11:32:31 +0530

    Prepare app for deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { DatabaseSync } = require("node:sqlite");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const db = new DatabaseSync("goals.db");
 
@@ -14,7 +14,7 @@ db.exec(`
 `);
 
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static("public"));
 
 app.get("/api/welcome", function (request, response) {
   response.json({
